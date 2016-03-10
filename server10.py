38cee758443b1c9336394561217e3b4efb13c0d9
mnikolnikov <mnikolnikov@yandex.ru>
2016-03-10 13:30:02 +0400

    Update server10.py

--- server10.py
+++ server10.py
@@ -1,11 +1,31 @@
+import asyncore
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('0.0.0.0', 2222))
-s.listen(10)
-while True:
-    conn, addr = s.accept()
-    while True:
-       data = conn.recv(1024)
-       if not data: break
-       conn.send(data)
-    conn.close()
+
+class EchoHandler(asyncore.dispatcher_with_send):
+
+    def handle_read(self):
+        data = self.recv(1024)
+        if data:
+            if data == 'close':
+                self.close()
+            else:
+                self.send(data)
+
+class EchoServer(asyncore.dispatcher):
+
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind((host, port))
+        self.listen(10)
+
+    def handle_accept(self):
+        pair = self.accept()
+        if pair is not None:
+            sock, addr = pair
+            print 'Incoming connection from %s' % repr(addr)
+            handler = EchoHandler(sock)
+
+server = EchoServer('0.0.0.0', 2222)
+asyncore.loop()
