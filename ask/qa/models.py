32d96074fe7558b965f8da3c394e6ed2264eac2a
mnikolnikov <mnikolnikov@yandex.ru>
2016-03-23 16:18:26 +0300

    12

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,18 +1,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField
-	added_at = models.DataTimeField
-	rating = models.IntegerField
+	added_at = models.DateTimeField
+	rating = models.IntegerField(default=0)
 	author = models.ForeignKey(User)
 	likes = models.ManyToManyField(User, related_name='likes_set')
 
 class Answer(models.Model):
 	text = models.TextField
-	added_at = models.DataTimeField
+	added_at = models.DateTimeField
 	question = models.ForeignKey(Question)
 	author = models.ForeignKey(User)
 		
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import Http404
+
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
-
+def popular(request, *args, **kwargs):
+	try:
+		pass
+	except Exception, e:
+		raise Http404
 # Create your views here.
